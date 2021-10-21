47546eab3104086426ad1a48330ea5075a5a4d08
AlmggmlA <aprigioluiz@gmail.com>
2021-10-21 17:16:29 -0300

    index

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
 
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-	<center><strong>CAFÉ DA MANHÃ MV</strong></center>
-	<img  src="/src/main/webapp/images/cafe_manha.jpg">
-	
-	<input type="hidden" name="x" value="1">
-	
-	faça o login <a href="/src/main/webapp/login.jsp">CLIQUE AQUI!</a><br/>
-	Se não tem cadastro, então <a href="/src/main/webapp/cadastroUsuario.jsp">CADASTRE-SE</a>
-	
 
-</body>
-</html>
 
 <?php include_once("/src/main/webapp/index.html"); ?>
